63a020d5c0112f1b8928ca7fcbc00122a44eb534
Helio Machabane <helio.machabane@gmail.com>
2020-03-28 19:54:48 +0200

    Desenhar o LRDA em PDF e EXCEL

---
+++ src/model/manager/reports/RegistoDiario.java
@@ -0,0 +1,114 @@
+package model.manager.reports;
+
+public class RegistoDiario {
+	
+	private String patientIdentifier;
+	
+	private String nome;
+	
+	private String zeroA4;
+	
+	private String cincoANove;
+	
+	private String dezACatorze;
+	
+	private String quinzeMais;
+	
+	private String tipoTarv;
+	
+	private String regimeTerapeutico;
+	
+	private String tipoDispensa;
+	
+	private String dataLevantamento;
+	
+	private String dataProximoLevantamento;
+
+	public String getPatientIdentifier() {
+		return patientIdentifier;
+	}
+
+	public void setPatientIdentifier(String patientIdentifier) {
+		this.patientIdentifier = patientIdentifier;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getZeroA4() {
+		return zeroA4;
+	}
+
+	public void setZeroA4(String zeroA4) {
+		this.zeroA4 = zeroA4;
+	}
+
+	public String getCincoANove() {
+		return cincoANove;
+	}
+
+	public void setCincoANove(String cincoANove) {
+		this.cincoANove = cincoANove;
+	}
+
+	public String getDezACatorze() {
+		return dezACatorze;
+	}
+
+	public void setDezACatorze(String dezACatorze) {
+		this.dezACatorze = dezACatorze;
+	}
+
+	public String getQuinzeMais() {
+		return quinzeMais;
+	}
+
+	public void setQuinzeMais(String quinzeMais) {
+		this.quinzeMais = quinzeMais;
+	}
+
+	public String getTipoTarv() {
+		return tipoTarv;
+	}
+
+	public void setTipoTarv(String tipoTarv) {
+		this.tipoTarv = tipoTarv;
+	}
+
+	public String getRegimeTerapeutico() {
+		return regimeTerapeutico;
+	}
+
+	public void setRegimeTerapeutico(String regimeTerapeutico) {
+		this.regimeTerapeutico = regimeTerapeutico;
+	}
+
+	public String getTipoDispensa() {
+		return tipoDispensa;
+	}
+
+	public void setTipoDispensa(String tipoDispensa) {
+		this.tipoDispensa = tipoDispensa;
+	}
+
+	public String getDataLevantamento() {
+		return dataLevantamento;
+	}
+
+	public void setDataLevantamento(String dataLevantamento) {
+		this.dataLevantamento = dataLevantamento;
+	}
+
+	public String getDataProximoLevantamento() {
+		return dataProximoLevantamento;
+	}
+
+	public void setDataProximoLevantamento(String dataProximoLevantamento) {
+		this.dataProximoLevantamento = dataProximoLevantamento;
+	}
+}
