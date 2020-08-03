c9cdcccc6c01a32670d0e60876faeaeaf672bbdf
colaconhongo <colaco.nhango@fgh.org.mz>
2020-08-03 16:15:36 +0200

    MMIA em  EXCEL

---
+++ src/model/manager/reports/MmiaRegimeTerapeuticoXLS.java
@@ -0,0 +1,12 @@
+package model.manager.reports;
+
+public class MmiaRegimeTerapeuticoXLS {
+
+    private String codigo;
+
+    private String regimeTerapeutico;
+
+    private String totalDoentes;
+
+    private String totalDoentesFarmaciaComunitaria;
+}
---
+++ src/model/manager/reports/MmiaStockXLS.java
@@ -0,0 +1,22 @@
+package model.manager.reports;
+
+public class MmiaStockXLS {
+
+    private String fnm;
+
+    private String medicamento;
+
+    private String quantidadeFrasco;
+
+    private String saldo;
+
+    private String entrdas;
+
+    private String saidas;
+
+    private String perdasAjustes;
+
+    private String inventario;
+
+    private String validade;
+}
